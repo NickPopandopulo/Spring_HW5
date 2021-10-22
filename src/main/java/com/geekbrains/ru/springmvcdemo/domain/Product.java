9f6a0d0a84df7d1b7f0653cf14c7b555fe466631
Nikita Popandopulo <nikas.popandopulos@gmail.com>
2021-10-22 17:09:28 +0700

    Homework 5

--- src/main/java/com/geekbrains/ru/springmvcdemo/domain/Product.java
+++ src/main/java/com/geekbrains/ru/springmvcdemo/domain/Product.java
@@ -2,18 +2,76 @@ package com.geekbrains.ru.springmvcdemo.domain;
 
 import lombok.*;
 
-@Data
-@Builder
-@NoArgsConstructor
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
+
     private String description;
+
+    @Column(name = "image_link")
     private String imageLink;
+
     private int price;
 
+    @Transient
     private Category category;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
